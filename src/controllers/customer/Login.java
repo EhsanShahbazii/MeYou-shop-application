75135e9f78efaa2cbfb6b64d54834cdc382a2d2e
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-03 02:18:14 +0330

    add ActionEvent for sigin and login pages buttons and add back to login page button in sign in pages

--- src/controllers/customer/Login.java
+++ src/controllers/customer/Login.java
@@ -3,7 +3,16 @@ package controllers.customer;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class Login {
 
@@ -19,4 +28,21 @@ public class Login {
     @FXML
     private JFXButton createNewAccountButton;
 
+    @FXML
+    void createNewAccount(ActionEvent event) throws IOException {
+        Stage primaryStage = (Stage) createNewAccountButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../../pages/customer/SigninPage.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Sign in");
+        primaryStage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.show();
+    }
+
+    @FXML
+    void login(ActionEvent event) throws IOException {
+
+    }
+
 }
+
--- src/controllers/customer/Signin.java
+++ src/controllers/customer/Signin.java
@@ -4,7 +4,15 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Signin {
 
@@ -31,6 +39,25 @@ public class Signin {
 
     @FXML
     private JFXTextArea addressTextArea;
+
+    @FXML
+    private JFXButton backToLogin;
+
+    @FXML
+    void backToLogin(ActionEvent event) throws IOException {
+        Stage primaryStage = (Stage) backToLogin.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../../pages/customer/LoginPage.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.show();
+    }
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
 
 }
 
