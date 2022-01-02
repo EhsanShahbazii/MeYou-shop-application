020aed3affbbe1f32d6575c672c6ed80036604b0
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-03 01:47:00 +0330

    add sign in pages and css files

---
+++ src/controllers/customer/Login.java
@@ -0,0 +1,22 @@
+package controllers.customer;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class Login {
+
+    @FXML
+    private JFXTextField userNameTextField;
+
+    @FXML
+    private JFXButton LoginButton;
+
+    @FXML
+    private JFXPasswordField passwordTextField;
+
+    @FXML
+    private JFXButton createNewAccountButton;
+
+}
