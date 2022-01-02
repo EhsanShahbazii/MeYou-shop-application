5b14ea4c245a5a3d51fe1e431f0acac7f529727b
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-03 01:52:54 +0330

    add controller classes for sign in and login pages

---
+++ src/controllers/customer/Signin.java
@@ -0,0 +1,36 @@
+package controllers.customer;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class Signin {
+
+    @FXML
+    private JFXTextField emailTextField;
+
+    @FXML
+    private JFXButton createAccountButton;
+
+    @FXML
+    private JFXTextField phoneNumberTextField;
+
+    @FXML
+    private JFXTextField fullNameTextField;
+
+    @FXML
+    private JFXTextField usernameTextField;
+
+    @FXML
+    private JFXPasswordField passwordTextField;
+
+    @FXML
+    private JFXPasswordField repeatPasswordTextField;
+
+    @FXML
+    private JFXTextArea addressTextArea;
+
+}
+
