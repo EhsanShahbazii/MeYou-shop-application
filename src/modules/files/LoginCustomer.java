b82e0214c5c965352018d4c377f8fef47d9d3aa9
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-03 22:37:38 +0330

    add comments in GlobalTools, LoginCustomer and Login classes

--- src/modules/files/LoginCustomer.java
+++ src/modules/files/LoginCustomer.java
@@ -7,43 +7,43 @@ import java.util.Scanner;
 public class LoginCustomer {
 
     public boolean loginCheck(String username, String password) {
-        int i = 0, count = 0;
+        int index = 0, lineCount = 0, correctIndex = 0;
         boolean state = false;
 
         try {
             //count lines of files in userInformation file
             Scanner scanner1 = new Scanner(new File("D:\\project final\\src\\files\\data\\usersInformation.txt"));
             while (scanner1.hasNextLine()) {
-                count++;
+                lineCount++;
                 scanner1.nextLine();
             }
             scanner1.close();
 
-            String[] counter = new String[count];
-            int k = 0;
+            //add username and password in array
+            String[] counter = new String[lineCount];
             Scanner scanner2 = new Scanner(new File("D:\\project final\\src\\files\\data\\usersInformation.txt"));
             while (scanner2.hasNextLine()) {
-                counter[i] = scanner2.nextLine();
-                i++;
+                counter[index] = scanner2.nextLine();
+                index++;
             }
             scanner2.close();
 
-            for (int j = 0; j < count; j++) {
+            //find correct index which one is equals with data
+            for (int j = 0; j < lineCount; j++) {
                 if (username.equals(counter[j]) && password.equals(counter[j+1])) {
-                    k = j;
+                    correctIndex = j;
                 }
             }
-
-            if (username.equals(counter[k]) && password.equals(counter[k+1])){
+            //check data is correct or not
+            if (username.equals(counter[correctIndex]) && password.equals(counter[correctIndex+1])){
                 state = true;
-            }else {
-                state = false;
             }
 
         }catch (FileNotFoundException error) {
             error.printStackTrace();
         }
 
+        //send state and show this data is true or not
         return state;
     }
 }
