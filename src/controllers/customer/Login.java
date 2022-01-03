b0d63485127bfd26d3c1a9342a2c6825d14cbaa6
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-03 21:22:34 +0330

    add limit char method for text fields in login pages

--- src/controllers/customer/Login.java
+++ src/controllers/customer/Login.java
@@ -3,11 +3,14 @@ package controllers.customer;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -15,6 +18,10 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class Login {
+
+    public void initialize() {
+
+    }
 
     @FXML
     private JFXTextField userNameTextField;
@@ -52,5 +59,18 @@ public class Login {
         primaryStage.show();
     }
 
+    //this method used for limited characters in textField
+    public void LimitedTextField(TextField textField, int maxLength){
+
+        textField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (textField.getText().length() > maxLength) {
+                    String s = textField.getText().substring(0, maxLength);
+                    textField.setText(s);
+                }
+            }
+        });
+    }
 }
 
