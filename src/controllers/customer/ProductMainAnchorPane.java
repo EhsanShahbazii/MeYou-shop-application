d89096ad3dc79f21ac0a4b4675a996958def230c
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-03 20:05:42 +0330

    create product main anchorPane fxml files and fixed bugs

---
+++ src/controllers/customer/ProductMainAnchorPane.java
@@ -0,0 +1,44 @@
+package controllers.customer;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class ProductMainAnchorPane {
+
+    @FXML
+    private AnchorPane productMainAnchorPane;
+
+    @FXML
+    private ImageView te;
+
+    @FXML
+    private Text pri;
+
+    @FXML
+    private Text productPriceText;
+
+    @FXML
+    private JFXButton productIncreaseCounter;
+
+    @FXML
+    private JFXButton productDecreaseCounter;
+
+    @FXML
+    private Text productCountText;
+
+    @FXML
+    void productIncreaseCounterAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void productecreaseCounterAction(ActionEvent event) {
+
+    }
+
+}
+
