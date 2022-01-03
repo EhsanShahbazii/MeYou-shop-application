e1efef4f89cdc59414ac4cc1a023ecfdd2a6acaa
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-03 19:40:35 +0330

    create product anchorpane for main pages and suggests pages

---
+++ src/controllers/customer/ProductAnchorPane.java
@@ -0,0 +1,19 @@
+package controllers.customer;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ProductAnchorPane {
+
+    @FXML
+    private Text priceText;
+
+    @FXML
+    private Text detailsText;
+
+    @FXML
+    private ImageView imageViewProduct;
+
+}
+
