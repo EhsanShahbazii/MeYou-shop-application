62049d2f77a02127fbf3d7fadda19c8432897825
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-06 23:25:30 +0330

    add setter and getter in productMainAnchorPane

--- src/controllers/customer/ProductMainAnchorPane.java
+++ src/controllers/customer/ProductMainAnchorPane.java
@@ -12,6 +12,70 @@ public class ProductMainAnchorPane {
     @FXML
     private AnchorPane productMainAnchorPane;
 
+    public AnchorPane getProductMainAnchorPane() {
+        return productMainAnchorPane;
+    }
+
+    public void setProductMainAnchorPane(AnchorPane productMainAnchorPane) {
+        this.productMainAnchorPane = productMainAnchorPane;
+    }
+
+    public ImageView getTe() {
+        return te;
+    }
+
+    public void setTe(ImageView te) {
+        this.te = te;
+    }
+
+    public Text getPri() {
+        return pri;
+    }
+
+    public void setPri(Text pri) {
+        this.pri = pri;
+    }
+
+    public Text getProductPriceText() {
+        return productPriceText;
+    }
+
+    public void setProductPriceText(Text productPriceText) {
+        this.productPriceText = productPriceText;
+    }
+
+    public JFXButton getProductIncreaseCounter() {
+        return productIncreaseCounter;
+    }
+
+    public void setProductIncreaseCounter(JFXButton productIncreaseCounter) {
+        this.productIncreaseCounter = productIncreaseCounter;
+    }
+
+    public JFXButton getProductDecreaseCounter() {
+        return productDecreaseCounter;
+    }
+
+    public void setProductDecreaseCounter(JFXButton productDecreaseCounter) {
+        this.productDecreaseCounter = productDecreaseCounter;
+    }
+
+    public Text getProductCountText() {
+        return productCountText;
+    }
+
+    public void setProductCountText(Text productCountText) {
+        this.productCountText = productCountText;
+    }
+
+    public JFXButton getBuyItButton() {
+        return buyItButton;
+    }
+
+    public void setBuyItButton(JFXButton buyItButton) {
+        this.buyItButton = buyItButton;
+    }
+
     @FXML
     private ImageView te;
 
