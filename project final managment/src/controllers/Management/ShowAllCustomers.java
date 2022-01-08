f38a5fadf9b63ade5ef2e6dca67d06cd9ba9938b
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-08 23:52:01 +0330

    create show all customers page and create controllers for it

---
+++ project final managment/src/controllers/Management/ShowAllCustomers.java
@@ -0,0 +1,21 @@
+package controllers.Management;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ShowAllCustomers {
+
+    @FXML
+    private JFXButton exitButton;
+
+    @FXML
+    private TableView<?> customerTable;
+
+    @FXML
+    void exitAction(ActionEvent event) {
+
+    }
+
+}
