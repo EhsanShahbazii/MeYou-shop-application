bd31012e214e926e4bc60d7343183ae951415279
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-08 18:29:57 +0330

    add and fixed management page and directory and files name

--- project final managment/src/controllers/Login.java
+++ project final managment/src/controllers/Login.java
@@ -5,6 +5,13 @@ import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Login {
 
@@ -21,8 +28,17 @@ public class Login {
     private JFXButton loginButton;
 
     @FXML
-    void LoginAction(ActionEvent event) {
-
+    void LoginAction(ActionEvent event) throws IOException {
+        Stage primaryStage = (Stage) loginButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../pages/managment/managementPage.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("./pages/css/homePageStyle.css");
+        primaryStage.setTitle("managment");
+        primaryStage.setScene(scene);
+        primaryStage.setX(450);
+        primaryStage.setY(110);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.show();
     }
 
     @FXML
---
+++ project final managment/src/controllers/Management/Management.java
@@ -0,0 +1,88 @@
+package controllers.Management;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class Management {
+
+    @FXML
+    private JFXButton showAllCustomersButton;
+
+    @FXML
+    private JFXButton showAllAdminsButton;
+
+    @FXML
+    private JFXButton showAllDocumentButton;
+
+    @FXML
+    private JFXButton addNewDiscountButton;
+
+    @FXML
+    private JFXButton addNewAdminButton;
+
+    @FXML
+    private JFXButton showAllProductButton;
+
+    @FXML
+    private LineChart<?, ?> lineChart;
+
+    @FXML
+    private PieChart pieChart;
+
+    @FXML
+    private Button changeImageButton;
+
+    @FXML
+    private ImageView profileImageView;
+
+    @FXML
+    private Text fullNameTextField;
+
+    @FXML
+    private Text timeTextField;
+
+    @FXML
+    private Text dateTextField;
+
+    @FXML
+    void addNewAdminAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addNewDiscountAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeProfileImageAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAllAdminsAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAllCustomersButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAllDocumentButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAllProductAction(ActionEvent event) {
+
+    }
+
+}
