5f083ac07f11444e557cf601263070fdb3b9ea4c
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-08 23:53:51 +0330

    create show all admins pages and create controllers for it

---
+++ project final managment/src/controllers/Management/ShowAllAdmins.java
@@ -0,0 +1,21 @@
+package controllers.Management;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ShowAllAdmins {
+
+    @FXML
+    private JFXButton exitButton;
+
+    @FXML
+    private TableView<?> adminTable;
+
+    @FXML
+    void exitAction(ActionEvent event) {
+
+    }
+
+}
