9a4da9370c3c6598becc62d83ede193d70c5abcb
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-08 22:34:02 +0330

    create add new admin page and add controller for it

---
+++ project final managment/src/controllers/Management/AddNewAdmin.java
@@ -0,0 +1,49 @@
+package controllers.Management;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AddNewAdmin {
+
+    @FXML
+    private JFXTextField emailTextField;
+
+    @FXML
+    private JFXButton addAdminButton;
+
+    @FXML
+    private JFXButton exitButton;
+
+    @FXML
+    private JFXTextField phoneNumberTextField;
+
+    @FXML
+    private JFXTextField usernameTextField;
+
+    @FXML
+    private JFXTextField fullNameTextField;
+
+    @FXML
+    private JFXTextArea addressTextArea;
+
+    @FXML
+    private JFXPasswordField passwordTextField;
+
+    @FXML
+    private JFXPasswordField repeatPasswordTextField;
+
+    @FXML
+    void addAdminAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitAction(ActionEvent event) {
+
+    }
+
+}
