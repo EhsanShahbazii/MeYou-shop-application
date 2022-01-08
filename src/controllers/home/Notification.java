92181e63b0bcae893411b6a56a240ee1d04f2822
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-08 14:30:14 +0330

    Add notification anchor and controlles

---
+++ src/controllers/home/Notification.java
@@ -0,0 +1,29 @@
+package controllers.home;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class Notification {
+
+    @FXML
+    private AnchorPane notificationScrollAnchor;
+
+    @FXML
+    private JFXButton closeNotificationButton;
+
+    @FXML
+    private JFXButton deleteNotificationButton;
+
+    @FXML
+    void closeNotificationAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteNotificationAction(ActionEvent event) {
+
+    }
+
+}
