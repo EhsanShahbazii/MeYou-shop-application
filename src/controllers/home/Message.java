7e4b74d96b3b149343c23654f635d586a48213dc
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-08 14:45:11 +0330

    create message pane and added to project, create controllers for it

---
+++ src/controllers/home/Message.java
@@ -0,0 +1,30 @@
+package controllers.home;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class Message {
+
+    @FXML
+    private AnchorPane messageScrollAnchor;
+
+    @FXML
+    private JFXButton closeMessageButton;
+
+    @FXML
+    private JFXButton deleteMessageButton;
+
+    @FXML
+    void closeMessageAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteMessageAction(ActionEvent event) {
+
+    }
+
+}
+
