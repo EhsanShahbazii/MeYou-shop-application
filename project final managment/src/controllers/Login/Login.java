48acefed3686f9e7b1e833760a71251c6988b3bc
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-09 00:24:28 +0330

    create show all products in admin page and create controllers for it

--- project final managment/src/controllers/Login/Login.java
+++ project final managment/src/controllers/Login/Login.java
@@ -1,4 +1,4 @@
-package controllers;
+package controllers.Login;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
---
+++ project final managment/src/controllers/admin/ShowAllProductsAdmin.java
@@ -0,0 +1,21 @@
+package controllers.admin;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ShowAllProductsAdmin {
+
+    @FXML
+    private JFXButton exitButton;
+
+    @FXML
+    private TableView<?> productTable;
+
+    @FXML
+    void exitAction(ActionEvent event) {
+
+    }
+
+}
