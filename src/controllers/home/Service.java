53d3c270d55e1abbc035b5b401d00a28ad07d437
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-08 16:06:32 +0330

    create service pane and create controllers for it

---
+++ src/controllers/home/Service.java
@@ -0,0 +1,17 @@
+package controllers.home;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Service {
+
+    @FXML
+    private JFXButton backServiceButton;
+
+    @FXML
+    void backServiceAction(ActionEvent event) {
+
+    }
+
+}
