03ce6fd6de78cda6c68376e4a9ec8d3c6a1aa650
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-09 12:31:06 +0330

    create admin page and create controller for it

---
+++ project final managment/src/controllers/admin/Admin.java
@@ -0,0 +1,72 @@
+package controllers.admin;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class Admin {
+
+    @FXML
+    private JFXButton addProductButton;
+
+    @FXML
+    private JFXButton deleteProductButton;
+
+    @FXML
+    private JFXButton chargeProductButton;
+
+    @FXML
+    private TableView<?> allProductTable;
+
+    @FXML
+    private JFXTextField productNewCountTextField;
+
+    @FXML
+    private JFXButton showProductButton1;
+
+    @FXML
+    private Button changeImageButton;
+
+    @FXML
+    private ImageView profileImageView;
+
+    @FXML
+    private Text fullNameTextField;
+
+    @FXML
+    private Text timeTextField;
+
+    @FXML
+    private Text dateTextField;
+
+    @FXML
+    void addProductAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeProfileImageAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chargeProductAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteProductAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showProductAction(ActionEvent event) {
+
+    }
+
+}
