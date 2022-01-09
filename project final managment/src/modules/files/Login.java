81ac8dc4f1ea52bdbc32a2af330e18c2d8154384
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-09 13:16:05 +0330

    add management and admin file

--- project final managment/src/modules/files/Login.java
+++ project final managment/src/modules/files/Login.java
@@ -6,31 +6,29 @@ public class Login {
 
     public GlobalFileTools globalFileTools = new GlobalFileTools();
 
-    public boolean loginCheck(String username, String password) {
+    public boolean loginCheck(String username, String password, String toggle) {
         int index = 0, lineCount = 0;
         boolean state = false;
 
         //count lines of files in userInformation file
-        lineCount = globalFileTools.fileLengthCounter("D:\\project final\\src\\files\\data\\usernameAndPassword.txt");
+        lineCount = globalFileTools.fileLengthCounter("D:\\project final\\project final managment\\src\\files\\data\\managementAndAdminLogin.txt");
 
         //add username and password in array
         int correctIndex = 0;
-        String[] counter = globalFileTools.fileAllRead("D:\\project final\\src\\files\\data\\usernameAndPassword.txt");
+        String[] counter = globalFileTools.fileAllRead("D:\\project final\\project final managment\\src\\files\\data\\managementAndAdminLogin.txt");
 
         //find correct index which one is equals with data
         //j = j+2 because odd lines are username and even lines are passwords
         for (int j = 0; j < lineCount-1; j = j+2) {
-            if (username.equals(counter[j]) && password.equals(counter[j+1])) {
+            if (username.equals(counter[j]) && password.equals(counter[j+1]) && toggle.equals(counter[j+2])) {
                 correctIndex = j;
             }
         }
         //check data is correct or not
-        if (username.equals(counter[correctIndex]) && password.equals(counter[correctIndex+1])){
+        if (username.equals(counter[correctIndex]) && password.equals(counter[correctIndex+1]) && toggle.equals(counter[correctIndex])){
             state = true;
         }
         //send state and show this data is true or not
         return state;
     }
-
-    
 }
