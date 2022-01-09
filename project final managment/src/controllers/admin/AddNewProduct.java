95cfbb79cc28b46699be0183b77adcfd726b67f9
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-09 12:45:34 +0330

    create add new product page and create controllers for it

---
+++ project final managment/src/controllers/admin/AddNewProduct.java
@@ -0,0 +1,54 @@
+package controllers.admin;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AddNewProduct {
+
+    @FXML
+    private JFXTextField imageTextField;
+
+    @FXML
+    private JFXButton addProductButton;
+
+    @FXML
+    private JFXButton exitButton;
+
+    @FXML
+    private JFXTextField authorTextField;
+
+    @FXML
+    private JFXTextField productNameTextField;
+
+    @FXML
+    private JFXTextArea detailTextArea;
+
+    @FXML
+    private JFXTextField countTextField;
+
+    @FXML
+    private JFXTextField priceTextField;
+
+    @FXML
+    private JFXButton chooseImageButton;
+
+    @FXML
+    void addProductAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chooseImageAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitAction(ActionEvent event) {
+
+    }
+
+}
+
