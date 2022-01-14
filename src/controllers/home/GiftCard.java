af2616de31258de434cb98a7a2593fbecb1ed346
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-14 14:19:27 +0330

    complete gift card classes and add some comments

--- src/controllers/home/GiftCard.java
+++ src/controllers/home/GiftCard.java
@@ -7,11 +7,13 @@ import modules.tools.GlobalTools;
 
 public class GiftCard {
 
+    //variables which are used
     GlobalTools globalTools = new GlobalTools();
 
     @FXML
     private JFXButton closeGiftCardAnchor;
 
+    //closeGiftCardAnchor and get back
     @FXML
     void closeGiftCardAnchorAction(ActionEvent event) {
         globalTools.closeCurrentPage(closeGiftCardAnchor);
